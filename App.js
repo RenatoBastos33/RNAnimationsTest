28408f7ed55aa79b8e6081d522b1b9356a287c6e
renatobastos33 <renato_bastos@id.uff.br>
2020-03-11 14:50:56 -0300

    Back animations

--- App.js
+++ App.js
@@ -1,27 +1,37 @@
 import * as React from 'react';
-import { Text, View, TouchableWithoutFeedback, TouchableOpacity, SafeAreaView, Dimensions, Image, Animated, ScrollView, StyleSheet } from 'react-native';
+import { Text, View, TouchableWithoutFeedback, TouchableOpacity, SafeAreaView, Dimensions, Image, Animated, ScrollView, StyleSheet, BackHandler } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createStackNavigator, useHeaderHeight } from '@react-navigation/stack';
+import { createStackNavigator, useHeaderHeight, CardStyleInterpolators, HeaderBackButton } from '@react-navigation/stack';
+import { TransitionPresets } from '@react-navigation/stack';
 
 
 function HomeScreen({ route }) {
-
-
-  const [imageOldPosition] = React.useState(route.params.imagePosition)
-  const [imageAnimation] = React.useState(new Animated.Value(-1))
-  const [scrollAnimation] = React.useState(new Animated.Value(0))
-  const [headerAnimation] = React.useState(new Animated.Value(0))
-
-  const [offsetScroll, setoffsetScroll] = React.useState(0)
-  const [headerTransparent, setHeaderTransparent] = React.useState(true)
 
   const imageScale = Dimensions.get('window').width / route.params.imagePosition.width
   const image = route.params.image
   const headerHeight = useHeaderHeight()
   const navigation = useNavigation()
 
+
+  const [imageOldPosition] = React.useState({ ...route.params.imagePosition, y: route.params.imagePosition.y })
+  const [imageAnimation] = React.useState(new Animated.Value(0))
+  const [scrollAnimation] = React.useState(new Animated.Value(0))
+  const [headerAnimation] = React.useState(new Animated.Value(0))
+  console.log('ImagePosition!', imageOldPosition, headerHeight)
+
+  const [offsetScroll, setoffsetScroll] = React.useState(0)
+  const [headerTransparent, setHeaderTransparent] = React.useState(true)
+
+  const goBackAnimation = () => {
+    Animated.parallel([
+      Animated.timing(imageAnimation, { toValue: 0, duration: 500, useNativeDriver: true }),
+      Animated.timing(scrollAnimation, { toValue: 0, duration: 500, useNativeDriver: true })
+    ]).start(() => navigation.goBack())
+  }
+
   React.useEffect(() => {
     navigation.setOptions({
+      headerLeft: ({ tintColor }) => (<HeaderBackButton tintColor={tintColor} onPress={() => goBackAnimation()} />),
       headerBackground: () => <Animated.View style={[{
         elevation: headerAnimation.interpolate({
           inputRange: [0, 100],
@@ -33,11 +43,32 @@ function HomeScreen({ route }) {
         })
       }, StyleSheet.absoluteFill]} />, headerTransparent: true
     })
-    Animated.timing(imageAnimation, { toValue: 0, duration: 500, useNativeDriver: true }).start()
+    Animated.timing(imageAnimation, { toValue: 1, duration: 500, useNativeDriver: true }).start()
   }, [])
 
   React.useEffect(() => {
+    BackHandler.addEventListener(
+      'hardwareBackPress',
+      handleBackButtonPressAndroid
+    );
+    return () => {
+      BackHandler.removeEventListener(
+        'hardwareBackPress',
+        handleBackButtonPressAndroid
+      );
+    };
+  }, []);
+  const handleBackButtonPressAndroid = () => {
+    if (!navigation.isFocused()) {
+      console.log('Entrou no if')
+      return false;
+    }
+    console.log('Não entrou no if')
+    goBackAnimation()
+    return true;
+  };
 
+  React.useEffect(() => {
     if (headerTransparent) {
       if (offsetScroll > (imageOldPosition.height * imageScale) - headerHeight) {
         navigation.setOptions({ headerTintColor: 'black' })
@@ -69,19 +100,19 @@ function HomeScreen({ route }) {
             transform: [
               {
                 translateY: imageAnimation.interpolate({
-                  inputRange: [-1, 0],
-                  outputRange: [imageOldPosition.y, ((imageOldPosition.height * imageScale) - imageOldPosition.height) / 2]
+                  inputRange: [0, 1],
+                  outputRange: [imageOldPosition.y + offsetScroll, ((imageOldPosition.height * imageScale) - imageOldPosition.height) / 2]
                 })
               },
               {
                 translateX: imageAnimation.interpolate({
-                  inputRange: [-1, 0],
+                  inputRange: [0, 1],
                   outputRange: [imageOldPosition.x, ((imageOldPosition.width * imageScale) - imageOldPosition.width) / 2]
                 })
               },
               {
                 scale: imageAnimation.interpolate({
-                  inputRange: [-1, 0],
+                  inputRange: [0, 1],
                   outputRange: [1, imageScale]
                 })
               }
@@ -91,7 +122,7 @@ function HomeScreen({ route }) {
 
       <Animated.View style={{
         alignItems: 'center',
-        opacity: imageAnimation.interpolate({ inputRange: [-1, 0], outputRange: [0, 1] })
+        opacity: imageAnimation
       }} >
         <Text style={{ marginBottom: 50 }}>SCROLL VIEW!</Text>
         <Text style={{ marginBottom: 50 }}>SCROLL VIEW!</Text>
@@ -134,10 +165,10 @@ const Stack = createStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator >
+      <Stack.Navigator screenOptions={{ animationEnabled: false }} >
         <Stack.Screen options={{ headerTransparent: true, headerTintColor: 'black' }} name="Home" component={SettingsScreen} />
         <Stack.Screen options={{ headerTransparent: true, headerTintColor: 'white' }} name="Descricao" component={HomeScreen} />
       </Stack.Navigator>
-    </NavigationContainer>
+    </NavigationContainer >
   );
 }
